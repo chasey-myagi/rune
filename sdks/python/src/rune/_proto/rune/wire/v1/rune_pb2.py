chasey-myagi/rune

commit 827b73c3891cbfd489680e824362e872b85e538d

fix: CI failures — retry test and SDK proto sync
- engine.rs: retry_on_any_retries_all_errors used vec![] (now-invalid
  empty retry_on); fix to vec![RetryCondition::Any]
- sdks/typescript/proto/rune.proto: add protocol_version = 7 field
  to CasterAttach (missed in v1.3.1 feature commits)
- sdks/python/_proto: regenerate from updated proto

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

sdks/python/src/rune/_proto/rune/wire/v1/rune_pb2.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
 # Generated by the protocol buffer compiler.  DO NOT EDIT!
+# NO CHECKED-IN PROTOBUF GENCODE
 # source: rune/wire/v1/rune.proto
-# Protobuf Python Version: 4.25.1
+# Protobuf Python Version: 6.31.1
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
+from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
+_runtime_version.ValidateProtobufRuntimeVersion(
+    _runtime_version.Domain.PUBLIC,
+    6,
+    31,
+    1,
+    '',
+    'rune/wire/v1/rune.proto'
+)
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -14,65 +24,65 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17rune/wire/v1/rune.proto\x12\x0crune.wire.v1\"\xe0\x04\n\x0eSessionMessage\x12,\n\x06\x61ttach\x18\n \x01(\x0b\x32\x1a.rune.wire.v1.CasterAttachH\x00\x12,\n\x06\x64\x65tach\x18\x0b \x01(\x0b\x32\x1a.rune.wire.v1.CasterDetachH\x00\x12-\n\x06result\x18\x0c \x01(\x0b\x32\x1b.rune.wire.v1.ExecuteResultH\x00\x12\x31\n\x0cstream_event\x18\r \x01(\x0b\x32\x19.rune.wire.v1.StreamEventH\x00\x12-\n\nstream_end\x18\x0e \x01(\x0b\x32\x17.rune.wire.v1.StreamEndH\x00\x12\x33\n\rhealth_report\x18\x0f \x01(\x0b\x32\x1a.rune.wire.v1.HealthReportH\x00\x12-\n\nattach_ack\x18\x14 \x01(\x0b\x32\x17.rune.wire.v1.AttachAckH\x00\x12/\n\x07\x65xecute\x18\x15 \x01(\x0b\x32\x1c.rune.wire.v1.ExecuteRequestH\x00\x12-\n\x06\x63\x61ncel\x18\x16 \x01(\x0b\x32\x1b.rune.wire.v1.CancelRequestH\x00\x12\x31\n\x0cscale_signal\x18\x17 \x01(\x0b\x32\x19.rune.wire.v1.ScaleSignalH\x00\x12\x31\n\x08shutdown\x18\x18 \x01(\x0b\x32\x1d.rune.wire.v1.ShutdownRequestH\x00\x12,\n\theartbeat\x18\x1e \x01(\x0b\x32\x17.rune.wire.v1.HeartbeatH\x00\x42\t\n\x07payload\"\xe9\x01\n\x0c\x43\x61sterAttach\x12\x11\n\tcaster_id\x18\x01 \x01(\t\x12,\n\x05runes\x18\x02 \x03(\x0b\x32\x1d.rune.wire.v1.RuneDeclaration\x12\x36\n\x06labels\x18\x03 \x03(\x0b\x32&.rune.wire.v1.CasterAttach.LabelsEntry\x12\x16\n\x0emax_concurrent\x18\x04 \x01(\r\x12\x0b\n\x03key\x18\x05 \x01(\t\x12\x0c\n\x04role\x18\x06 \x01(\t\x1a-\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"c\n\tAttachAck\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\x12\x0e\n\x06reason\x18\x02 \x01(\t\x12\x1a\n\x12supported_features\x18\x03 \x03(\t\x12\x18\n\x10protocol_version\x18\x04 \x01(\t\"\x1e\n\x0c\x43\x61sterDetach\x12\x0e\n\x06reason\x18\x01 \x01(\t\"\xc5\x01\n\x0fRuneDeclaration\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x14\n\x0cinput_schema\x18\x04 \x01(\t\x12\x15\n\routput_schema\x18\x05 \x01(\t\x12\x17\n\x0fsupports_stream\x18\x06 \x01(\x08\x12&\n\x04gate\x18\x07 \x01(\x0b\x32\x18.rune.wire.v1.GateConfig\x12\x10\n\x08priority\x18\x08 \x01(\x05\"*\n\nGateConfig\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x0e\n\x06method\x18\x02 \x01(\t\"C\n\x0e\x46ileAttachment\x12\x10\n\x08\x66ilename\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\x12\x11\n\tmime_type\x18\x03 \x01(\t\"\xf9\x01\n\x0e\x45xecuteRequest\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x11\n\trune_name\x18\x02 \x01(\t\x12\r\n\x05input\x18\x03 \x01(\x0c\x12:\n\x07\x63ontext\x18\x04 \x03(\x0b\x32).rune.wire.v1.ExecuteRequest.ContextEntry\x12\x12\n\ntimeout_ms\x18\x05 \x01(\r\x12\x31\n\x0b\x61ttachments\x18\x06 \x03(\x0b\x32\x1c.rune.wire.v1.FileAttachment\x1a.\n\x0c\x43ontextEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"3\n\rCancelRequest\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x0e\n\x06reason\x18\x02 \x01(\t\"\x8e\x01\n\x0bScaleSignal\x12)\n\x06\x61\x63tion\x18\x01 \x01(\x0e\x32\x19.rune.wire.v1.ScaleAction\x12\x10\n\x08group_id\x18\x02 \x01(\t\x12\x18\n\x10\x64\x65sired_replicas\x18\x03 \x01(\r\x12\x18\n\x10\x63urrent_pressure\x18\x04 \x01(\x01\x12\x0e\n\x06reason\x18\x05 \x01(\t\":\n\x0fShutdownRequest\x12\x0e\n\x06reason\x18\x01 \x01(\t\x12\x17\n\x0fgrace_period_ms\x18\x02 \x01(\r\"\xb6\x01\n\rExecuteResult\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12$\n\x06status\x18\x02 \x01(\x0e\x32\x14.rune.wire.v1.Status\x12\x0e\n\x06output\x18\x03 \x01(\x0c\x12(\n\x05\x65rror\x18\x04 \x01(\x0b\x32\x19.rune.wire.v1.ErrorDetail\x12\x31\n\x0b\x61ttachments\x18\x05 \x03(\x0b\x32\x1c.rune.wire.v1.FileAttachment\"C\n\x0bStreamEvent\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\x12\x12\n\nevent_type\x18\x03 \x01(\t\"o\n\tStreamEnd\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12$\n\x06status\x18\x02 \x01(\x0e\x32\x14.rune.wire.v1.Status\x12(\n\x05\x65rror\x18\x03 \x01(\x0b\x32\x19.rune.wire.v1.ErrorDetail\"!\n\tHeartbeat\x12\x14\n\x0ctimestamp_ms\x18\x01 \x01(\x04\"\xae\x02\n\x0cHealthReport\x12*\n\x06status\x18\x01 \x01(\x0e\x32\x1a.rune.wire.v1.HealthStatus\x12\x17\n\x0f\x61\x63tive_requests\x18\x02 \x01(\r\x12\x12\n\nerror_rate\x18\x03 \x01(\x02\x12\x13\n\x0b\x63ustom_info\x18\x04 \x01(\t\x12\x14\n\x0ctimestamp_ms\x18\x05 \x01(\x04\x12\x1e\n\x16\x65rror_rate_window_secs\x18\x06 \x01(\r\x12\x10\n\x08pressure\x18\x07 \x01(\x01\x12\x38\n\x07metrics\x18\x08 \x03(\x0b\x32\'.rune.wire.v1.HealthReport.MetricsEntry\x1a.\n\x0cMetricsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x01:\x02\x38\x01\"=\n\x0b\x45rrorDetail\x12\x0c\n\x04\x63ode\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\x12\x0f\n\x07\x64\x65tails\x18\x03 \x01(\x0c*_\n\x06Status\x12\x16\n\x12STATUS_UNSPECIFIED\x10\x00\x12\x14\n\x10STATUS_COMPLETED\x10\x01\x12\x11\n\rSTATUS_FAILED\x10\x02\x12\x14\n\x10STATUS_CANCELLED\x10\x03*\x81\x01\n\x0cHealthStatus\x12\x1d\n\x19HEALTH_STATUS_UNSPECIFIED\x10\x00\x12\x19\n\x15HEALTH_STATUS_HEALTHY\x10\x01\x12\x1a\n\x16HEALTH_STATUS_DEGRADED\x10\x02\x12\x1b\n\x17HEALTH_STATUS_UNHEALTHY\x10\x03*W\n\x0bScaleAction\x12\x1c\n\x18SCALE_ACTION_UNSPECIFIED\x10\x00\x12\x13\n\x0fSCALE_ACTION_UP\x10\x01\x12\x15\n\x11SCALE_ACTION_DOWN\x10\x02\x32X\n\x0bRuneService\x12I\n\x07Session\x12\x1c.rune.wire.v1.SessionMessage\x1a\x1c.rune.wire.v1.SessionMessage(\x01\x30\x01\x62\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17rune/wire/v1/rune.proto\x12\x0crune.wire.v1\"\xe0\x04\n\x0eSessionMessage\x12,\n\x06\x61ttach\x18\n \x01(\x0b\x32\x1a.rune.wire.v1.CasterAttachH\x00\x12,\n\x06\x64\x65tach\x18\x0b \x01(\x0b\x32\x1a.rune.wire.v1.CasterDetachH\x00\x12-\n\x06result\x18\x0c \x01(\x0b\x32\x1b.rune.wire.v1.ExecuteResultH\x00\x12\x31\n\x0cstream_event\x18\r \x01(\x0b\x32\x19.rune.wire.v1.StreamEventH\x00\x12-\n\nstream_end\x18\x0e \x01(\x0b\x32\x17.rune.wire.v1.StreamEndH\x00\x12\x33\n\rhealth_report\x18\x0f \x01(\x0b\x32\x1a.rune.wire.v1.HealthReportH\x00\x12-\n\nattach_ack\x18\x14 \x01(\x0b\x32\x17.rune.wire.v1.AttachAckH\x00\x12/\n\x07\x65xecute\x18\x15 \x01(\x0b\x32\x1c.rune.wire.v1.ExecuteRequestH\x00\x12-\n\x06\x63\x61ncel\x18\x16 \x01(\x0b\x32\x1b.rune.wire.v1.CancelRequestH\x00\x12\x31\n\x0cscale_signal\x18\x17 \x01(\x0b\x32\x19.rune.wire.v1.ScaleSignalH\x00\x12\x31\n\x08shutdown\x18\x18 \x01(\x0b\x32\x1d.rune.wire.v1.ShutdownRequestH\x00\x12,\n\theartbeat\x18\x1e \x01(\x0b\x32\x17.rune.wire.v1.HeartbeatH\x00\x42\t\n\x07payload\"\x83\x02\n\x0c\x43\x61sterAttach\x12\x11\n\tcaster_id\x18\x01 \x01(\t\x12,\n\x05runes\x18\x02 \x03(\x0b\x32\x1d.rune.wire.v1.RuneDeclaration\x12\x36\n\x06labels\x18\x03 \x03(\x0b\x32&.rune.wire.v1.CasterAttach.LabelsEntry\x12\x16\n\x0emax_concurrent\x18\x04 \x01(\r\x12\x0b\n\x03key\x18\x05 \x01(\t\x12\x0c\n\x04role\x18\x06 \x01(\t\x12\x18\n\x10protocol_version\x18\x07 \x01(\t\x1a-\n\x0bLabelsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"c\n\tAttachAck\x12\x10\n\x08\x61\x63\x63\x65pted\x18\x01 \x01(\x08\x12\x0e\n\x06reason\x18\x02 \x01(\t\x12\x1a\n\x12supported_features\x18\x03 \x03(\t\x12\x18\n\x10protocol_version\x18\x04 \x01(\t\"\x1e\n\x0c\x43\x61sterDetach\x12\x0e\n\x06reason\x18\x01 \x01(\t\"\xc5\x01\n\x0fRuneDeclaration\x12\x0c\n\x04name\x18\x01 \x01(\t\x12\x0f\n\x07version\x18\x02 \x01(\t\x12\x13\n\x0b\x64\x65scription\x18\x03 \x01(\t\x12\x14\n\x0cinput_schema\x18\x04 \x01(\t\x12\x15\n\routput_schema\x18\x05 \x01(\t\x12\x17\n\x0fsupports_stream\x18\x06 \x01(\x08\x12&\n\x04gate\x18\x07 \x01(\x0b\x32\x18.rune.wire.v1.GateConfig\x12\x10\n\x08priority\x18\x08 \x01(\x05\"*\n\nGateConfig\x12\x0c\n\x04path\x18\x01 \x01(\t\x12\x0e\n\x06method\x18\x02 \x01(\t\"C\n\x0e\x46ileAttachment\x12\x10\n\x08\x66ilename\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\x12\x11\n\tmime_type\x18\x03 \x01(\t\"\xf9\x01\n\x0e\x45xecuteRequest\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x11\n\trune_name\x18\x02 \x01(\t\x12\r\n\x05input\x18\x03 \x01(\x0c\x12:\n\x07\x63ontext\x18\x04 \x03(\x0b\x32).rune.wire.v1.ExecuteRequest.ContextEntry\x12\x12\n\ntimeout_ms\x18\x05 \x01(\r\x12\x31\n\x0b\x61ttachments\x18\x06 \x03(\x0b\x32\x1c.rune.wire.v1.FileAttachment\x1a.\n\x0c\x43ontextEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\"3\n\rCancelRequest\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x0e\n\x06reason\x18\x02 \x01(\t\"\x8e\x01\n\x0bScaleSignal\x12)\n\x06\x61\x63tion\x18\x01 \x01(\x0e\x32\x19.rune.wire.v1.ScaleAction\x12\x10\n\x08group_id\x18\x02 \x01(\t\x12\x18\n\x10\x64\x65sired_replicas\x18\x03 \x01(\r\x12\x18\n\x10\x63urrent_pressure\x18\x04 \x01(\x01\x12\x0e\n\x06reason\x18\x05 \x01(\t\":\n\x0fShutdownRequest\x12\x0e\n\x06reason\x18\x01 \x01(\t\x12\x17\n\x0fgrace_period_ms\x18\x02 \x01(\r\"\xb6\x01\n\rExecuteResult\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12$\n\x06status\x18\x02 \x01(\x0e\x32\x14.rune.wire.v1.Status\x12\x0e\n\x06output\x18\x03 \x01(\x0c\x12(\n\x05\x65rror\x18\x04 \x01(\x0b\x32\x19.rune.wire.v1.ErrorDetail\x12\x31\n\x0b\x61ttachments\x18\x05 \x03(\x0b\x32\x1c.rune.wire.v1.FileAttachment\"C\n\x0bStreamEvent\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12\x0c\n\x04\x64\x61ta\x18\x02 \x01(\x0c\x12\x12\n\nevent_type\x18\x03 \x01(\t\"o\n\tStreamEnd\x12\x12\n\nrequest_id\x18\x01 \x01(\t\x12$\n\x06status\x18\x02 \x01(\x0e\x32\x14.rune.wire.v1.Status\x12(\n\x05\x65rror\x18\x03 \x01(\x0b\x32\x19.rune.wire.v1.ErrorDetail\"!\n\tHeartbeat\x12\x14\n\x0ctimestamp_ms\x18\x01 \x01(\x04\"\xae\x02\n\x0cHealthReport\x12*\n\x06status\x18\x01 \x01(\x0e\x32\x1a.rune.wire.v1.HealthStatus\x12\x17\n\x0f\x61\x63tive_requests\x18\x02 \x01(\r\x12\x12\n\nerror_rate\x18\x03 \x01(\x02\x12\x13\n\x0b\x63ustom_info\x18\x04 \x01(\t\x12\x14\n\x0ctimestamp_ms\x18\x05 \x01(\x04\x12\x1e\n\x16\x65rror_rate_window_secs\x18\x06 \x01(\r\x12\x10\n\x08pressure\x18\x07 \x01(\x01\x12\x38\n\x07metrics\x18\x08 \x03(\x0b\x32\'.rune.wire.v1.HealthReport.MetricsEntry\x1a.\n\x0cMetricsEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\x01:\x02\x38\x01\"=\n\x0b\x45rrorDetail\x12\x0c\n\x04\x63ode\x18\x01 \x01(\t\x12\x0f\n\x07message\x18\x02 \x01(\t\x12\x0f\n\x07\x64\x65tails\x18\x03 \x01(\x0c*_\n\x06Status\x12\x16\n\x12STATUS_UNSPECIFIED\x10\x00\x12\x14\n\x10STATUS_COMPLETED\x10\x01\x12\x11\n\rSTATUS_FAILED\x10\x02\x12\x14\n\x10STATUS_CANCELLED\x10\x03*\x81\x01\n\x0cHealthStatus\x12\x1d\n\x19HEALTH_STATUS_UNSPECIFIED\x10\x00\x12\x19\n\x15HEALTH_STATUS_HEALTHY\x10\x01\x12\x1a\n\x16HEALTH_STATUS_DEGRADED\x10\x02\x12\x1b\n\x17HEALTH_STATUS_UNHEALTHY\x10\x03*W\n\x0bScaleAction\x12\x1c\n\x18SCALE_ACTION_UNSPECIFIED\x10\x00\x12\x13\n\x0fSCALE_ACTION_UP\x10\x01\x12\x15\n\x11SCALE_ACTION_DOWN\x10\x02\x32X\n\x0bRuneService\x12I\n\x07Session\x12\x1c.rune.wire.v1.SessionMessage\x1a\x1c.rune.wire.v1.SessionMessage(\x01\x30\x01\x62\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'rune.wire.v1.rune_pb2', _globals)
-if _descriptor._USE_C_DESCRIPTORS == False:
-  DESCRIPTOR._options = None
-  _globals['_CASTERATTACH_LABELSENTRY']._options = None
+if not _descriptor._USE_C_DESCRIPTORS:
+  DESCRIPTOR._loaded_options = None
+  _globals['_CASTERATTACH_LABELSENTRY']._loaded_options = None
   _globals['_CASTERATTACH_LABELSENTRY']._serialized_options = b'8\001'
-  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._options = None
+  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._loaded_options = None
   _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_options = b'8\001'
-  _globals['_HEALTHREPORT_METRICSENTRY']._options = None
+  _globals['_HEALTHREPORT_METRICSENTRY']._loaded_options = None
   _globals['_HEALTHREPORT_METRICSENTRY']._serialized_options = b'8\001'
-  _globals['_STATUS']._serialized_start=2614
-  _globals['_STATUS']._serialized_end=2709
-  _globals['_HEALTHSTATUS']._serialized_start=2712
-  _globals['_HEALTHSTATUS']._serialized_end=2841
-  _globals['_SCALEACTION']._serialized_start=2843
-  _globals['_SCALEACTION']._serialized_end=2930
+  _globals['_STATUS']._serialized_start=2640
+  _globals['_STATUS']._serialized_end=2735
+  _globals['_HEALTHSTATUS']._serialized_start=2738
+  _globals['_HEALTHSTATUS']._serialized_end=2867
+  _globals['_SCALEACTION']._serialized_start=2869
+  _globals['_SCALEACTION']._serialized_end=2956
   _globals['_SESSIONMESSAGE']._serialized_start=42
   _globals['_SESSIONMESSAGE']._serialized_end=650
   _globals['_CASTERATTACH']._serialized_start=653
-  _globals['_CASTERATTACH']._serialized_end=886
-  _globals['_CASTERATTACH_LABELSENTRY']._serialized_start=841
-  _globals['_CASTERATTACH_LABELSENTRY']._serialized_end=886
-  _globals['_ATTACHACK']._serialized_start=888
-  _globals['_ATTACHACK']._serialized_end=987
-  _globals['_CASTERDETACH']._serialized_start=989
-  _globals['_CASTERDETACH']._serialized_end=1019
-  _globals['_RUNEDECLARATION']._serialized_start=1022
-  _globals['_RUNEDECLARATION']._serialized_end=1219
-  _globals['_GATECONFIG']._serialized_start=1221
-  _globals['_GATECONFIG']._serialized_end=1263
-  _globals['_FILEATTACHMENT']._serialized_start=1265
-  _globals['_FILEATTACHMENT']._serialized_end=1332
-  _globals['_EXECUTEREQUEST']._serialized_start=1335
-  _globals['_EXECUTEREQUEST']._serialized_end=1584
-  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_start=1538
-  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_end=1584
-  _globals['_CANCELREQUEST']._serialized_start=1586
-  _globals['_CANCELREQUEST']._serialized_end=1637
-  _globals['_SCALESIGNAL']._serialized_start=1640
-  _globals['_SCALESIGNAL']._serialized_end=1782
-  _globals['_SHUTDOWNREQUEST']._serialized_start=1784
-  _globals['_SHUTDOWNREQUEST']._serialized_end=1842
-  _globals['_EXECUTERESULT']._serialized_start=1845
-  _globals['_EXECUTERESULT']._serialized_end=2027
-  _globals['_STREAMEVENT']._serialized_start=2029
-  _globals['_STREAMEVENT']._serialized_end=2096
-  _globals['_STREAMEND']._serialized_start=2098
-  _globals['_STREAMEND']._serialized_end=2209
-  _globals['_HEARTBEAT']._serialized_start=2211
-  _globals['_HEARTBEAT']._serialized_end=2244
-  _globals['_HEALTHREPORT']._serialized_start=2247
-  _globals['_HEALTHREPORT']._serialized_end=2549
-  _globals['_HEALTHREPORT_METRICSENTRY']._serialized_start=2503
-  _globals['_HEALTHREPORT_METRICSENTRY']._serialized_end=2549
-  _globals['_ERRORDETAIL']._serialized_start=2551
-  _globals['_ERRORDETAIL']._serialized_end=2612
-  _globals['_RUNESERVICE']._serialized_start=2932
-  _globals['_RUNESERVICE']._serialized_end=3020
+  _globals['_CASTERATTACH']._serialized_end=912
+  _globals['_CASTERATTACH_LABELSENTRY']._serialized_start=867
+  _globals['_CASTERATTACH_LABELSENTRY']._serialized_end=912
+  _globals['_ATTACHACK']._serialized_start=914
+  _globals['_ATTACHACK']._serialized_end=1013
+  _globals['_CASTERDETACH']._serialized_start=1015
+  _globals['_CASTERDETACH']._serialized_end=1045
+  _globals['_RUNEDECLARATION']._serialized_start=1048
+  _globals['_RUNEDECLARATION']._serialized_end=1245
+  _globals['_GATECONFIG']._serialized_start=1247
+  _globals['_GATECONFIG']._serialized_end=1289
+  _globals['_FILEATTACHMENT']._serialized_start=1291
+  _globals['_FILEATTACHMENT']._serialized_end=1358
+  _globals['_EXECUTEREQUEST']._serialized_start=1361
+  _globals['_EXECUTEREQUEST']._serialized_end=1610
+  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_start=1564
+  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_end=1610
+  _globals['_CANCELREQUEST']._serialized_start=1612
+  _globals['_CANCELREQUEST']._serialized_end=1663
+  _globals['_SCALESIGNAL']._serialized_start=1666
+  _globals['_SCALESIGNAL']._serialized_end=1808
+  _globals['_SHUTDOWNREQUEST']._serialized_start=1810
+  _globals['_SHUTDOWNREQUEST']._serialized_end=1868
+  _globals['_EXECUTERESULT']._serialized_start=1871
+  _globals['_EXECUTERESULT']._serialized_end=2053
+  _globals['_STREAMEVENT']._serialized_start=2055
+  _globals['_STREAMEVENT']._serialized_end=2122
+  _globals['_STREAMEND']._serialized_start=2124
+  _globals['_STREAMEND']._serialized_end=2235
+  _globals['_HEARTBEAT']._serialized_start=2237
+  _globals['_HEARTBEAT']._serialized_end=2270
+  _globals['_HEALTHREPORT']._serialized_start=2273
+  _globals['_HEALTHREPORT']._serialized_end=2575
+  _globals['_HEALTHREPORT_METRICSENTRY']._serialized_start=2529
+  _globals['_HEALTHREPORT_METRICSENTRY']._serialized_end=2575
+  _globals['_ERRORDETAIL']._serialized_start=2577
+  _globals['_ERRORDETAIL']._serialized_end=2638
+  _globals['_RUNESERVICE']._serialized_start=2958
+  _globals['_RUNESERVICE']._serialized_end=3046
 # @@protoc_insertion_point(module_scope)

sdks/python/src/rune/_proto/rune/wire/v1/rune_pb2_grpc.py
@@ -1,8 +1,28 @@
 # Generated by the gRPC Python protocol compiler plugin. DO NOT EDIT!
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
+import warnings
 
-from . import rune_pb2 as rune_dot_wire_dot_v1_dot_rune__pb2
+from rune.wire.v1 import rune_pb2 as rune_dot_wire_dot_v1_dot_rune__pb2
+
+GRPC_GENERATED_VERSION = '1.80.0'
+GRPC_VERSION = grpc.__version__
+_version_not_supported = False
+
+try:
+    from grpc._utilities import first_version_is_lower
+    _version_not_supported = first_version_is_lower(GRPC_VERSION, GRPC_GENERATED_VERSION)
+except ImportError:
+    _version_not_supported = True
+
+if _version_not_supported:
+    raise RuntimeError(
+        f'The grpc package installed is at version {GRPC_VERSION},'
+        + ' but the generated code in rune/wire/v1/rune_pb2_grpc.py depends on'
+        + f' grpcio>={GRPC_GENERATED_VERSION}.'
+        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
+        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
+    )
 
 
 class RuneServiceStub(object):
@@ -19,7 +39,7 @@ class RuneServiceStub(object):
                 '/rune.wire.v1.RuneService/Session',
                 request_serializer=rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.SerializeToString,
                 response_deserializer=rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.FromString,
-                )
+                _registered_method=True)
 
 
 class RuneServiceServicer(object):
@@ -44,6 +64,7 @@ def add_RuneServiceServicer_to_server(servicer, server):
     generic_handler = grpc.method_handlers_generic_handler(
             'rune.wire.v1.RuneService', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
+    server.add_registered_method_handlers('rune.wire.v1.RuneService', rpc_method_handlers)
 
 
  # This class is part of an EXPERIMENTAL API.
@@ -62,8 +83,18 @@ class RuneService(object):
             wait_for_ready=None,
             timeout=None,
             metadata=None):
-        return grpc.experimental.stream_stream(request_iterator, target, '/rune.wire.v1.RuneService/Session',
+        return grpc.experimental.stream_stream(
+            request_iterator,
+            target,
+            '/rune.wire.v1.RuneService/Session',
             rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.SerializeToString,
             rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)

runtime/rune-flow/src/engine.rs
@@ -1933,12 +1933,18 @@ mod tests {
         assert_eq!(call_count.load(std::sync::atomic::Ordering::SeqCst), 1);
     }
 
-    /// retry_on=[] 时所有错误都重试，应达到 max_attempts 次调用
+    /// retry_on=[Any] 时所有错误都重试，应达到 max_attempts 次调用
     #[tokio::test]
     async fn retry_on_any_retries_all_errors() {
-        use crate::dag::BackoffStrategy;
+        use crate::dag::{BackoffStrategy, RetryCondition};
         use rune_core::rune::RuneError;
-        let cfg = make_retry(3, 0, BackoffStrategy::Fixed, None, vec![]);
+        let cfg = make_retry(
+            3,
+            0,
+            BackoffStrategy::Fixed,
+            None,
+            vec![RetryCondition::Any],
+        );
         let call_count = std::sync::Arc::new(std::sync::atomic::AtomicU32::new(0));
         let cc = call_count.clone();
         let result = retry_op(

sdks/typescript/proto/rune.proto
@@ -38,6 +38,7 @@ message CasterAttach {
     uint32 max_concurrent = 4;
     string key = 5;              // Caster 认证密钥（dev 模式下可省略）
     string role = 6;             // "caster" (default) | "pilot"
+    string protocol_version = 7; // Caster 声明的协议版本（为空则兼容旧版）
 }
 
 message AttachAck {

commit 171d32b644a74e3011c3113102f4e7367069e0eb

fix: regenerate Python proto using pinned grpcio-tools==1.62.3
The sync-proto.sh script pins grpcio-tools==1.62.3 (protobuf 4.25.1).
827b73c accidentally committed files generated with protobuf 6.31.1,
causing the CI proto-sync diff check to fail. Regenerate with the
correct toolchain version to match CI expectations.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

sdks/python/src/rune/_proto/rune/wire/v1/rune_pb2.py
@@ -1,22 +1,12 @@
 # -*- coding: utf-8 -*-
 # Generated by the protocol buffer compiler.  DO NOT EDIT!
-# NO CHECKED-IN PROTOBUF GENCODE
 # source: rune/wire/v1/rune.proto
-# Protobuf Python Version: 6.31.1
+# Protobuf Python Version: 4.25.1
 """Generated protocol buffer code."""
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
-from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-_runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC,
-    6,
-    31,
-    1,
-    '',
-    'rune/wire/v1/rune.proto'
-)
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -29,13 +19,13 @@ DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x17rune/wire/v1/r
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'rune.wire.v1.rune_pb2', _globals)
-if not _descriptor._USE_C_DESCRIPTORS:
-  DESCRIPTOR._loaded_options = None
-  _globals['_CASTERATTACH_LABELSENTRY']._loaded_options = None
+if _descriptor._USE_C_DESCRIPTORS == False:
+  DESCRIPTOR._options = None
+  _globals['_CASTERATTACH_LABELSENTRY']._options = None
   _globals['_CASTERATTACH_LABELSENTRY']._serialized_options = b'8\001'
-  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._loaded_options = None
+  _globals['_EXECUTEREQUEST_CONTEXTENTRY']._options = None
   _globals['_EXECUTEREQUEST_CONTEXTENTRY']._serialized_options = b'8\001'
-  _globals['_HEALTHREPORT_METRICSENTRY']._loaded_options = None
+  _globals['_HEALTHREPORT_METRICSENTRY']._options = None
   _globals['_HEALTHREPORT_METRICSENTRY']._serialized_options = b'8\001'
   _globals['_STATUS']._serialized_start=2640
   _globals['_STATUS']._serialized_end=2735

sdks/python/src/rune/_proto/rune/wire/v1/rune_pb2_grpc.py
@@ -1,28 +1,8 @@
 # Generated by the gRPC Python protocol compiler plugin. DO NOT EDIT!
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
-import warnings
 
-from rune.wire.v1 import rune_pb2 as rune_dot_wire_dot_v1_dot_rune__pb2
-
-GRPC_GENERATED_VERSION = '1.80.0'
-GRPC_VERSION = grpc.__version__
-_version_not_supported = False
-
-try:
-    from grpc._utilities import first_version_is_lower
-    _version_not_supported = first_version_is_lower(GRPC_VERSION, GRPC_GENERATED_VERSION)
-except ImportError:
-    _version_not_supported = True
-
-if _version_not_supported:
-    raise RuntimeError(
-        f'The grpc package installed is at version {GRPC_VERSION},'
-        + ' but the generated code in rune/wire/v1/rune_pb2_grpc.py depends on'
-        + f' grpcio>={GRPC_GENERATED_VERSION}.'
-        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
-        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
-    )
+from . import rune_pb2 as rune_dot_wire_dot_v1_dot_rune__pb2
 
 
 class RuneServiceStub(object):
@@ -39,7 +19,7 @@ class RuneServiceStub(object):
                 '/rune.wire.v1.RuneService/Session',
                 request_serializer=rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.SerializeToString,
                 response_deserializer=rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.FromString,
-                _registered_method=True)
+                )
 
 
 class RuneServiceServicer(object):
@@ -64,7 +44,6 @@ def add_RuneServiceServicer_to_server(servicer, server):
     generic_handler = grpc.method_handlers_generic_handler(
             'rune.wire.v1.RuneService', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
-    server.add_registered_method_handlers('rune.wire.v1.RuneService', rpc_method_handlers)
 
 
  # This class is part of an EXPERIMENTAL API.
@@ -83,18 +62,8 @@ class RuneService(object):
             wait_for_ready=None,
             timeout=None,
             metadata=None):
-        return grpc.experimental.stream_stream(
-            request_iterator,
-            target,
-            '/rune.wire.v1.RuneService/Session',
+        return grpc.experimental.stream_stream(request_iterator, target, '/rune.wire.v1.RuneService/Session',
             rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.SerializeToString,
             rune_dot_wire_dot_v1_dot_rune__pb2.SessionMessage.FromString,
-            options,
-            channel_credentials,
-            insecure,
-            call_credentials,
-            compression,
-            wait_for_ready,
-            timeout,
-            metadata,
-            _registered_method=True)
+            options, channel_credentials,
+            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)